504f2d9b71d6a77d4c872b386394fe45b3f9e32d
Huy NGUYEN <huu_nhat_huy.nguyen@insa-cvl.fr>
2023-11-15 04:53:13 +0100

    Revert "Add Utilities file"
    This reverts commit fe91ba1bad4be4de19e8aec55585b6341eebd3e0.

--- Classes.py
+++ Classes.py
@@ -1,5 +1,6 @@
 from enum import Enum, auto
 import numpy as np
+import main
 
 
 
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import tkinter.messagebox
+import Classes
 from tkinter import *
 import tkinter as tk
 from ttkbootstrap.constants import *
 import ttkbootstrap as tb
-import Utilities
 
 root = tb.Window(themename= "cosmo")
 #App interface:
@@ -17,7 +17,6 @@ licenses.grid(row = 1)
 
 ###     INITIALISATION OF VARIABLES:
 #Initialisation of 2D list called sem:
-import Classes
 sem = Classes.Semaine()
 
 #Import list of subjects:
@@ -39,10 +38,53 @@ my_notebook.add(tab3, text="Déplacer une séance")
 
 ### AJOUTER SEANCES
 
-import Utilities
-
 ##Fonctions:
-Utilities.ajouteSeance()
+
+#getIdSeance takes a string which is the name of seance and convert it to index of it in listM
+def getIdSeance(nom:str):
+    arr = []
+    for s in matieres.listeM:
+        arr.append(s)
+    for temp in arr:
+        if nom==str(temp):
+            return arr.index(temp)
+        else:
+            return 99
+
+def printSeances():
+    arr=[]
+    for s in matieres.listeM:
+        arr.append(s)
+    for temp in arr:
+        print(temp)
+
+def checkType(id:int):
+    if id==1:
+        return 'CM'
+    elif id==2:
+        return 'TD'
+    else:
+        return 'TP'
+
+def convertJour(s:str):
+    index = Classes.jours.index(s)
+    return index + 1
+
+def ajouteSeance():
+###Pick up values:
+    #Check if any field is missing:
+    if combo_seance.get() != '' and r.get() != 0 and seance1.get() != 0 and jour1.get() != '':
+        id_matiere = getIdSeance(combo_seance.get())        #an integer define id of the seance
+        type_seance = checkType(r.get())                    #string ('CM', 'TD', 'TP')
+        num_seance= int(seance1.get())-1                    #number of the seance in the day
+        num_jour = convertJour(jour1.get())                 #an integer from 1 to 7 indicate from Monday to Sunday
+        num_semaine = int(semaine1.get())                              #number of week
+    #if one or more fields are missing, send error
+    else:
+        tk.messagebox.showerror(message="Please fill in all the info!", title="Error")
+
+    sem.numS[num_semaine][num_seance*num_jour]= Classes.Seance(id=id_matiere, hCM=10)
+    label1.config(text=f'{sem.numS[num_semaine][num_seance*num_jour]} ajoute! and nom {combo_seance.get()}')
 
 #Add seance
 lable_seance=tb.Label(tab1, text="Selecter la seance:", font=('Arial', 11, 'italic'))
@@ -61,13 +103,9 @@ def Clicked(value):
     #tk.messagebox.showinfo(title="Radiobutton", message=f'{value} clicked')
     pass
 
-radio_button1=tb.Radiobutton(tab1, text="CM  ", bootstyle="secondary", variable=r, value=1, command=lambda: Clicked(r.get()))
-radio_button2=tb.Radiobutton(tab1, text="TD  ", bootstyle="secondary", variable=r, value=2, command=lambda: Clicked(r.get()))
-radio_button3=tb.Radiobutton(tab1, text="TP  ", bootstyle="secondary", variable=r, value=3, command=lambda: Clicked(r.get()))
-
-radio_button1.grid(row=3, column=5)
-radio_button2.grid(row=3, column=6)
-radio_button3.grid(row=3, column=7)
+tb.Radiobutton(tab1, text="CM  ", bootstyle="secondary", variable=r, value=1, command=lambda: Clicked(r.get())).grid(row=3, column=5)
+tb.Radiobutton(tab1, text="TD  ", bootstyle="secondary", variable=r, value=2, command=lambda: Clicked(r.get())).grid(row=3, column=6)
+tb.Radiobutton(tab1, text="TP  ", bootstyle="secondary", variable=r, value=3, command=lambda: Clicked(r.get())).grid(row=3, column=7)
 
 lable_semaine=tb.Label(tab1, text="Saisir la semaine: ", font=('Arial', 11, 'italic'))
 lable_semaine.grid(row=4, pady=10)
@@ -87,7 +125,7 @@ lable_numSeance.grid(row=6, pady=10)
 seance1=tb.Combobox(tab1, bootstyle="secondary", values=[1,2,3,4])
 seance1.grid(row=6, column=1)
 
-button1 = tb.Button(tab1, text="Ajouter", bootstyle="primary", command=lambda: Utilities.ajouteSeance())
+button1 = tb.Button(tab1, text="Ajouter", bootstyle="primary", command=lambda: ajouteSeance())
 button1.grid(row=7, column=3, pady=10)
 
 ### Testing
@@ -96,8 +134,8 @@ label1.grid(row=8)
 
 root.mainloop()
 
-print(Utilities.getIdSeance("MAEL"))
-Utilities.printSeances()
+print(getIdSeance("MAEL"))
+printSeances()
 #print(combo_seance)
 #sem.numS[1][0] = Classes.Seance(id=1, hCM=2)
 print(sem.numS[1][0])
